415163dda70fa29887aa90b75d7505a7a1b6e40a
RafatSh-Hash <rafatsh800@gmail.com>
2023-08-30 03:31:13 +0600

    "How it works" complete

--- styling.js
+++ styling.js
@@ -6,27 +6,30 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const n = 1;
-//   const cardContainer = document.querySelector(".card-container");
+(function () {
+  var rotate, timeline;
 
-//   for (let i = 1; i <= n; i++) {
-//     const card = document.createElement("a");
-//     card.classList.add("card");
-//     card.href = "#!";
-//     card.innerHTML = `
-//       <div class="front" style="background-image: url(//source.unsplash.com/300x40${i})">
-//         <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-//       </div>
-//       <div class="back">
-//         <div>
-//           <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-//           <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
-//           <button class="button">Click Here</button>
-//         </div>
-//       </div>
-//     `;
+  rotate = function () {
+    var container = document.querySelector(".container");
+    var firstCard = container.firstElementChild;
 
-//     cardContainer.appendChild(card);
-//   }
-// });
+    firstCard.style.transition = "opacity 0.4s ease-in-out";
+    firstCard.style.opacity = 0;
+
+    setTimeout(function () {
+      container.appendChild(firstCard);
+      firstCard.style.opacity = 1;
+    }, 400);
+  };
+
+  timeline = setInterval(rotate, 1200);
+
+  document.body.addEventListener("mouseenter", function () {
+    clearInterval(timeline);
+  });
+
+  var cards = document.querySelectorAll(".cardC");
+  cards.forEach(function (card) {
+    card.addEventListener("click", rotate);
+  });
+})();
